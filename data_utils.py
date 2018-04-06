a0aebb85efcb8bd0e9aa27f929ada35e7cab3337
smahesh1 <smahesh3288@gmail.com>
2018-04-06 15:41:33 -0500

    added networkx for modularity calculations

--- data_utils.py
+++ data_utils.py
@@ -13,6 +13,12 @@ import matplotlib.pyplot as plt
 from skimage.color import rgb2gray
 from scipy import stats
 import glob
+import matplotlib.pyplot as plt
+import seaborn as sn
+import pandas as pd
+import time
+from tqdm import tqdm
+import skimage
 
 
 ### DATA LOADING ###
@@ -40,15 +46,12 @@ class Mask():
 		self.dir_id = dir_id
 
 
-
-
-
 # Load all the train images
 
 def get_train_images():
 	#images = np.array([])
 	images = []
-	for path in glob.iglob('stage1_train/*/images/*'):
+	for path in tqdm(glob.iglob('stage1_train/*/images/*')):
 		im = imageio.imread(path)
 		#im = rgb2gray(im)
 
@@ -63,7 +66,7 @@ def get_train_images():
 def get_train_masks():
 	#masks = np.array([])
 	masks = []
-	for path in glob.iglob('stage1_train/*/masks/*'):
+	for path in tqdm(glob.iglob('stage1_train/*/masks/*')):
 		im = imageio.imread(path)
 		#im = rgb2gray(im)
 		#masks = np.append(masks, im, axis=0)
@@ -76,7 +79,7 @@ def get_train_masks():
 def get_test_images():
 	#images = np.array([])
 	images = []
-	for path in glob.iglob('stage1_test/*/images/*.png'):
+	for path in tqdm(glob.iglob('stage1_test/*/images/*.png')):
 		im = imageio.imread(path)
 		#im = rgb2gray(im)
 
@@ -85,6 +88,20 @@ def get_test_images():
 		images.append(full_image)
 	return images
 
+def preprocess_image(image):
+	# Step 2. Change image to black and white
+
+	bw = rgb2gray(image.im)
+	
+	# Step 2. polarizatton correction , we want the mean to less than 0.5
+	mu = np.mean(bw)
+	image.im = bw
+	if mu > 0.5:
+		image.im = 1-bw
+
+
+
+
 
 # Given a mask image, returns the coordinates of nuclei
 # i.e. pixels that are non-0
@@ -92,5 +109,137 @@ def get_nuclei_pixels(image):
     return np.argwhere(image != 0)
 
 
-get_train_images()
 
+def get_total_mask(image, train_masks):
+	associated_masks = []
+	for m in tqdm(train_masks):
+		if m.dir_id == image.dir_id:
+			associated_masks.append(m)
+	return combine_masks(associated_masks)
+
+
+# given a list of individual nuclei masks, combine them into a 
+# single mask image
+def combine_masks(mask_list):
+	mask = mask_list[0].im
+	for m in mask_list:
+		mask += m.im
+
+	mask[mask > 0] = 1
+	mask[mask <0]=0
+	return mask
+
+
+# Breaks up an image and mask pari into sub images/masks.
+# This is super helpful to help normalize the data size, as 
+# well as to create more data.
+def convolve(image,mask,dim=128,sample_size=100):
+	a = rgb2gray(image.im)
+	sub_shape = (dim, dim)
+	view_shape = tuple(np.subtract(a.shape, sub_shape) + 1) + sub_shape
+	strides = a.strides + a.strides
+	#strides = [10,11,12,13]
+	sub_matrices = np.lib.stride_tricks.as_strided(a,view_shape,strides)
+
+
+	# reshape
+	#num_images = sub_matrices.shape[0]*sub_matrices.shape[1]
+	#sub_matrices = sub_matrices.reshape(num_images,dim,dim);
+
+	am = mask
+	sub_shapem = (dim, dim)
+	view_shapem = tuple(np.subtract(mask.shape, sub_shapem) + 1) + sub_shapem
+	stridesm = am.strides + am.strides
+	sub_matricesm = np.lib.stride_tricks.as_strided(am,view_shapem,stridesm)
+
+	# reshape
+	rand_row_indices = np.random.randint(len(sub_matricesm), size = sample_size)
+	rand_col_indices = np.random.randint(len(sub_matricesm[0]), size = sample_size)
+
+	sample_images = sub_matrices[rand_row_indices, rand_col_indices]
+	sample_masks = sub_matricesm[rand_row_indices, rand_col_indices]
+
+	return sample_images, sample_masks
+
+
+### PIXEL ENCODING AND SUBMISSION ### 
+# source:
+# Sam Stainsby 
+# Fast,tested RLE and input routines
+# https://www.kaggle.com/stainsby/fast-tested-rle-and-input-routines
+
+# https://www.kaggle.com/akshayt19nayak/getting-started-image-processing-basics
+def encode(mask):
+
+    dots = np.where(mask.T.flatten()==1)[0] # .T sets order down-then-right
+    run_lengths = []
+    prev = -2
+    for b in tqdm(dots):
+        if (b > prev+1): run_lengths.extend((b+1, 0))
+        run_lengths[-1] += 1
+        prev = b
+    return " ".join([str(i) for i in run_lengths])
+
+
+# source : https://www.kaggle.com/stainsby/fast-tested-rle-and-input-routines
+def rle_to_string(runs):
+    return ' '.join(str(x) for x in runs)
+
+
+# source :https://www.kaggle.com/stainsby/fast-tested-rle-and-input-routines
+def rle_decode(rle_str, mask_shape, mask_dtype):
+    s = rle_str.split()
+    starts, lengths = [np.asarray(x, dtype=int) for x in (s[0:][::2], s[1:][::2])]
+    starts -= 1
+    ends = starts + lengths
+    mask = np.zeros(np.prod(mask_shape), dtype=mask_dtype)
+    for lo, hi in zip(starts, ends):
+        mask[lo:hi] = 1
+    return mask.reshape(mask_shape[::-1]).T
+
+
+# given an image id, returns a list of strings 
+# that given the rle encodedings of the predicted nuclei,
+# one string per nuclei
+def binary_label_encode(predicted_mask):
+	rles = []
+	# This submission code provided by https://www.kaggle.com/jruizvar/otsu-thresholding-segmentation
+	labeled_array, num_features = skimage.measure.label(predicted_mask, return_num=True)
+	for label in range(1, num_features+1):
+		mask = labeled_array == label
+		mask_encoded = encode(mask)
+		rles.append(str(mask_encoded)[1:-1])
+	return rles
+
+def make_submission(image_ids, predicted_masks):
+	if len(image_ids) != len(predicted_masks):
+		print "error, lengths don't match"
+		return -1
+
+	ids = []
+	rles = []
+
+	for i in tqdm(range(len(image_ids))):
+		im_id = image_ids[i]
+		im_id = im_id[im_id.rindex("/")+1:im_id.rindex(".")]
+		predicted_mask = predicted_masks[i]
+		encodings = binary_label_encode(predicted_mask)
+		for i in range(len(encodings)):
+			ids.append(im_id)
+			rles.append(encodings[i])
+
+	print len(ids)
+	print len(rles)
+	data = {"ImageId":ids,"EncodedPixels":rles}
+	df = pd.DataFrame.from_dict(data)
+	df = df[["ImageId","EncodedPixels"]]
+	df.to_csv("sumission_test.csv",index=False)
+
+
+
+### Plotting ###
+def plot_confusion_matrix(array):
+    df_cm = pd.DataFrame(array, index = [i for i in ["True Nuclei"," True Not-Nuclei"]],
+                      columns = [i for i in ["Predicted Nuclei","Predicted Not-Nuclei"]])
+    plt.figure(figsize = (10,7))
+    sn.heatmap(df_cm, annot=True)
